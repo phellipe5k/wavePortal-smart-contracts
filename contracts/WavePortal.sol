1f4a01237ddafd082411cb6039b1994957986063
Luiz Andrade <luizinhophe@hotmail.com>
2022-04-04 00:50:44 -0300

    fixing smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,42 +6,17 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalColors;
-    struct userInfo {
-        string name;
-        string email;
-        string favorite_color;
-    }
-    mapping(uint256 => userInfo) users;
-    uint256[] public userIds;
+
     constructor() {
         console.log("Hello, World! I am a contract");
     }
-/*
-    function wave() public {
-        totalWaves += 1;
-        console.log("Waved!", msg.sender);
-    }
-*/
     function getColorsMetrics() public view returns(uint256) {
         console.log("We have total waves!", totalColors);
         return totalColors;
     }
 
-    function registerUser(string memory name, string memory email, string memory favorite_color, uint256 id) public {
-        userInfo storage newUser = users[id];
-        newUser.name = name;
-        newUser.email = email;
-        newUser.favorite_color = favorite_color;
-        userIds.push(id);
-        totalColors++;
+    function registerColor() public {
+        totalColors += 1;
     }
 
-    function getUsers() public view returns (uint256) {
-        uint i = 0;
-        for (i; i < userIds.length; i++) {
-        console.log(users[i].name, "favorite color is ", users[i].favorite_color);
-        console.log("Email: ", users[i].email);
-        }
-        return userIds[i];
-    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,13 @@
+const { ethers } = require("hardhat");
+
 const main = async () => {
-    const [deployer] = await hre.ethers.getSigners();
+    const [deployer] = await ethers.getSigners();
     const accountBalance = await deployer.getBalance();
   
     console.log("Deploying contracts with account: ", deployer.address);
     console.log("Account balance: ", accountBalance.toString());
   
-    const Token = await hre.ethers.getContractFactory("WavePortal");
+    const Token = await ethers.getContractFactory("WavePortal");
     const portal = await Token.deploy();
     await portal.deployed();
   
