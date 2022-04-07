7ada168e6726dc2d58501b61d9dc700a8253d0b6
Luiz Andrade <luizinhophe@hotmail.com>
2022-04-06 22:31:56 -0300

    updating contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,11 +5,20 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    string[] public colors;
+    event NewVote(address indexed from, uint256 timestamp, string color);
+    
+    struct ColorVote {
+        address user;
+        string color;
+        uint256 timestamp;
+    }
+    ColorVote[] colors;
+
+    
     constructor() {
         console.log("Hello, World! I am a contract");
     }
-    function getColors() public view returns(string[] memory) {
+    function getColors() public view returns(ColorVote[] memory) {
         return colors;
     }
 
@@ -20,7 +29,9 @@ contract WavePortal {
     
 
     function registerColor(string memory color) public {
-        colors.push(color);
+        colors.push(ColorVote(msg.sender, color, block.timestamp));
+
+        emit NewVote(msg.sender, block.timestamp, color);
     }
 
 }
